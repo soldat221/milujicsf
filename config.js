41bf6d660238c819aacbaafcca875a71daceabfe
Pavel Horal <pavel.horal@orchitech.cz>
2020-10-21 18:29:41 +0200

    BZX

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const audioFiles = [
   ["BZX", "To je debil!", "files/bzx_1.01.mp3", 0.5],
+  ["BZX", "AAAAAAAA!", "files/bzx_1.02.mp3", 0.5],
+  ["BZX", "Kreténskej idiot!", "files/bzx_1.03.mp3", 0.5],
 ];
