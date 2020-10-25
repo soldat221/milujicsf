68b29da9edfa1b4a11a39047facb508a6a840aee
soldat221 <73298141+soldat221@users.noreply.github.com>
2020-10-25 13:29:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('static')
       .then(function (cache) {
-        return cache.addAll(['/',
+        return cache.addAll([/*'/',*/
                       'index.html',
                       '.editorconfig',
                       'browserconfig.xml',
