a8464b3eb401f3337c855e169627d54de9957e2d
soldat221 <73298141+soldat221@users.noreply.github.com>
2020-10-25 13:46:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,6 +21,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('static')
       .then(function (cache) {
+        cache.add('style.css');
         return cache.addAll([/*'/',*/
                       'index.html',
                       '.editorconfig',
